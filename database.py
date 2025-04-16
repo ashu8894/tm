a9702f5d7be9a024069e12ba93739ec97dc58420
Ashutosh Renu <162429580+ashu8894@users.noreply.github.com>
2025-04-16 19:20:12 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://admin:admin123@127.0.0.1:5432/truckdb"
+DATABASE_URL = "postgresql://truckdb_user:4L1V94qsWljZLBIma4JwLP78puYY9JBQ@dpg-cvvr8cuuk2gs73djt0t0-a.virginia-postgres.render.com/truckdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
